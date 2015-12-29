edbe393239790e33aa69cb69a036781df75bfde8
Charlie Allom <charlie@yelp.com>
2015-12-29 21:16:04 +1000

    import asnconv.c, my first C.

--- README.md
+++ README.md
@@ -1,10 +1,21 @@
 I can never remember how to convert the bits, so here are two bash scripts to symlink to ~/bin in case you need to convert between asdot and asplain.
 
+In `bash`.
 ```
 % dot2plain 65044:21
 4262723605
 % plain2dot 4262723605
 65044:21
+```
+
+or
+
+This is a more complete C version, probably just as broken.
+```
+% make asnconv
+% ./asnconv 4262723631
+% ./asnconv 65036.12
+% ./asnconv 65412
 ```
 
 asdot notation is deprecated, btw :)
---
+++ asnconv.c
@@ -0,0 +1,47 @@
+/*  convert BGP ASNs
+*  ./asnconv 4262723631
+*  ./asnconv 65036.12
+*  ./asnconv 65412
+*/
+ 
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+int main(int argc, char *argv[])
+{
+  //char *asn = malloc(strlen(argv[1]) );
+  char asn[strlen(argv[1])] ;
+  strcpy(asn, argv[1]);
+  char dot = '.';
+  char *asdot = strchr(asn, dot);
+  unsigned int asn_int = atoi(asn);
+
+  if(asdot != NULL ) { // if '.' is found, then we have asdot
+
+    printf("ASDOT: ");
+    /* wtaf http://stackoverflow.com/a/19724503 */
+    *asdot = '\0'; // overwrite the test, creating two strings (XXX)
+    unsigned int asn_low = asn_int;
+    unsigned int asn_high = atoi(asdot+1); // +1 is magic to find next string
+    printf("%u\n", (asn_low<<16)+asn_high); // multiply by 65536
+    printf("(%d*65535+%d)\n", asn_low, asn_high);
+    printf("low: %d\nhigh: %d\n", asn_low, asn_high);
+
+  } else if(asn_int <= 65535 ) { // normal low asn
+
+    printf("2 Byte ASPLAIN: ");
+    printf("%u", asn_int);
+    printf("\n");
+    //printf("(%u)\n", asn);
+
+  } else if(asn_int >= 65536) { // otherwise, asplain
+
+    printf("4 Byte ASPLAIN: ");
+    printf("%u.%u", asn_int/65536, asn_int%65536);
+    printf("\n");
+    printf("(%u)\n", asn_int);
+  }
+
+    return (0);
+}
